a987396918222a09660d8c41f112a49e4f8c77f9
juliavarin <juliamariavarin@gmail.com>
2024-07-09 14:36:52 -0300

    aula 9

---
+++ aula9/dino.py
@@ -0,0 +1,13 @@
+import pyautogui
+
+pyautogui.click(200,200, interval=.5)
+pyautogui.press('space')
+
+while True:
+    if pyautogui.pixelMatchesColor(232,430,[83,83,83], 70):
+        print(pyautogui.pixel(232,430))
+        pyautogui.press('space')
+
+    if pyautogui.pixelMatchesColor(232,395,[83,83,83], 70):
+        print(pyautogui.pixel(232,395))
+        pyautogui.press('down', interval=2)
